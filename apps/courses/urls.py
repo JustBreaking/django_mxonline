d6fb9a6c42465b0669472d9eae7a7e6b45cad843
justbreaking <mgzsin@qq.com>
2017-07-02 01:39:09 +0800

    delete _xadmin

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import url, include
 from .views import CourseListView, CourseDetailView, CourseCaptherView, CourseCOmmentView, AddCommentView, VideoPlayView
 
 urlpatterns = [
-    #课程列表页
+    # 课程列表页
     url(r'^list/$', CourseListView.as_view(), name='course_list'),
     url(r'^detail/(?P<course_id>\d+)/$', CourseDetailView.as_view(), name='course_detail'),
     url(r'^capther/(?P<course_id>\d+)/$', CourseCaptherView.as_view(), name='course_capther'),
